01e93bf719ad7d7861c73b7110c5adeda1c04a21
phannhat17 <phandinhnhat1708@gmail.com>
2024-03-16 23:00:51 +0700

    add start script and add adv search

--- application/app.py
+++ application/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from pymetasploit3.msfrpc import *
+import os
 
 app = Flask(__name__)
 
-client = MsfRpcClient('yourpassword', port=55553, ssl=True)
+msfpass = os.getenv('MSFPASS', 'yourpassword')  
+
+client = MsfRpcClient(msfpass, port=55553, ssl=True)
 
 @app.route('/')
 def index():
@@ -14,24 +17,81 @@ def index():
     total = len(results)
 
     if not search_query and (filter_type is None or filter_type == "all"):
-        total = str(len(results)) + " modules but the table only display first 500 modules"
+        total = str(len(results)) + " modules but the table only display first 500"
         results = results[:500]
+    elif filter_type and filter_type != "all":
+        results = [r for r in results if r['type'] == filter_type]
+        total = len(results)
+
+    return render_template('index.html', rows=results, total=total, filter_type=filter_type, search_query=search_query)
+
+@app.route('/adv-search', methods=['GET', 'POST'])
+def adv_search():
+    if request.method == 'POST':
+        name = request.form.get('name', None)
+        fullname = request.form.get('fullname', None)
+        aka = request.form.get('aka', None)
+        date = request.form.get('date', None)
+        target = request.form.get('target', None)
+        author = request.form.get('author', None)
+        m_type = request.form.get('type', None)
+        platform = request.form.get('platform', None)
+        arch = request.form.get('arch', None)
+        path = request.form.get('path', None)
+        port = request.form.get('port', None)
+        edb = request.form.get('edb', None)
+        bid = request.form.get('bid', None)
+        cve = request.form.get('cve', None)
+        reference = request.form.get('reference', None)
+        rank = request.form.get('rank', None)
+        check = request.form.get('check', 'false')
+
+        search_parts = []
+        if name:
+            search_parts.append("name:" + name)
+        if fullname:
+            search_parts.append("fullname:" + fullname)
+        if aka:
+            search_parts.append("aka:" + aka)
+        if date:
+            search_parts.append("date:" + date)
+        if target:
+            search_parts.append("target:" + target)
+        if author:
+            search_parts.append("author:" + author)
+        if m_type and m_type != 'all':
+            search_parts.append("type:" + m_type)
+        if platform:
+            search_parts.append("platform:" + platform)
+        if arch:
+            search_parts.append("arch:" + arch)
+        if path:
+            search_parts.append("path:" + path)
+        if port:
+            search_parts.append("port:" + port)
+        if edb:
+            search_parts.append("edb:" + edb)
+        if bid:
+            search_parts.append("bid:" + bid)
+        if cve:
+            search_parts.append("cve:" + cve)
+        if reference:
+            search_parts.append("reference:" + reference)
+        if rank:
+            search_parts.append("rank:" + rank)
+        if check != 'None':
+            search_parts.append("check:" + check)
+
+        search_query = ' '.join(search_parts)
+        if search_query == '':
+            results = []
+            return render_template('adv_search.html', toast=True, rows=results)
+        else:
+            results = client.modules.search(search_query)
+            return render_template('adv_search.html', rows=results)
     else:
-        if filter_type and filter_type != "all":
-            results = [r for r in results if r['type'] == filter_type]
-            total = len(results)
+        return render_template('adv_search.html')
 
-    return render_template('index.html', rows=results, total=total, filter_type=filter_type)
-
-# @app.route('/search', methods=['GET'])
-# def search():
-#     client = init_metasploit()
-#     search_query = request.args.get('query', '')
-#     results = client.modules.search(search_query)
-
-#     return render_template('index.html', rows=results)
-
-#     # return jsonify(formatted_results)
 
 @app.route('/module_details')
 def module_details():
