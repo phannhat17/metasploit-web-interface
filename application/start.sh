bea71732cf3f3c6f193fd0b0526c867f48e042a6
phannhat17 <phandinhnhat1708@gmail.com>
2024-03-22 15:00:02 +0700

    new init script

--- application/start.sh
+++ application/start.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-# Switching to virtual environment
+sudo systemctl start postgresql
+sudo msfdb init
+
 echo -e "\e[35m[*] Switching to venv\e[0m"
 source .venv/bin/activate
 
-# Generating msfRPC server password
-echo -e "\e[32m[*] Generating msfRPC server password\e[0m"
-export MSFPASS=$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 20)
-echo $MSFPASS
+# Check if msfRPC server is already running
+echo -e "\e[31m[*] Checking if msfRPC server is running..."
+if nc -z localhost 55553; then
+    echo -e "\e[31m[i] msf RPC server is already running. Killing the existing process.\e[0m"
+    # Kill the existing msfRPC server process
+    MSFRPC_PID=$(lsof -ti:55553)
+    if [ ! -z "$MSFRPC_PID" ]; then
+        sudo kill -9 $MSFRPC_PID
+        echo -e "\e[31m[i] Killed the existing msfRPC server process.\e[0m"
+    fi
+fi
+
+# Check and set MSFPASS
+if [ -z "${MSFPASS}" ]; then
+    echo -e "\e[32m[*] Generating msfRPC server password\e[0m"
+    export MSFPASS=$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 20)
+    echo "    Generated msfRPC server password: $MSFPASS"
+fi
 
 # Starting msfRPC server in the background
-echo -e "\e[34m[*] Starting MSGRPC server\e[0m"
-msfrpcd -P "$MSFPASS" &
+echo -e "\e[34m[*] Starting msfRPC server\e[0m"
+msfrpcd -P "$MSFPASS" &&
 
-# Wait for msfrpcd to be ready (listening on port 55553)
-echo "Waiting for msfRPC server to start..."
+echo -e "\e[34m[i] Waiting for msfRPC server to start..."
 while ! nc -z localhost 55553; do   
-  sleep 1 # Wait for 1 second before check again
+    sleep 1
 done
-echo "msfRPC server is up and running."
+echo -e "\e[34m[i] msfRPC server is up and running."
 
 # Starting Flask app
-echo -e "\e[36m[*] Starting flask app\e[0m"
+echo -e "\e[36m[*] Starting Flask app\e[0m"
 python app.py
-
